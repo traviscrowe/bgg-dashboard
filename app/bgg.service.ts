e135485ffac993a87149743d86db253c73c744ae
Travis Crowe <traviscrowe@gmail.com>
2016-03-11 10:05:44 -0600

    Added status code check for api requests

--- app/bgg.service.ts
+++ app/bgg.service.ts
@@ -21,7 +21,11 @@ export class BggService {
     }
     
     var url = 'https://cors-anywhere.herokuapp.com/http://www.boardgamegeek.com/xmlapi2/search?type=' + this.typeQuery + '&query=' + query.value;
-    return this._http.get(url, headers).map((res:Response) => res.text());
+    return this._http.get(url, headers).map((res:Response) =>  {
+        if(res.status < 200 || res.status >= 300) {
+            throw new Error('This request has failed ' + res.status);
+        } else return res.text();
+    });
   }
   
   getGamesByIds(ids) {
@@ -29,6 +33,10 @@ export class BggService {
     headers.append('Accept', 'application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8');
     
     var url = 'https://cors-anywhere.herokuapp.com/http://www.boardgamegeek.com/xmlapi2/thing?type=' + this.typeQuery + '&id=' + ids;
-    return this._http.get(url, headers).map((res:Response) => res.text());
+    return this._http.get(url, headers).map((res:Response) =>  {
+        if(res.status < 200 || res.status >= 300) {
+            throw new Error('This request has failed ' + res.status);
+        } else return res.text();
+    });
   }
 }
